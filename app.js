7676e279c8d37cfcbca1a48fdec7f7274a6d3607
fullpistoladev <47484699+fullpistoladev@users.noreply.github.com>
2022-04-26 21:41:09 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app.js
@@ -0,0 +1,19 @@
+
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu() {
+  
+  const header = document.querySelector("header");
+  header.classList.toggle('active');
+  
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+
+
+function removerMenu() {
+  const header = document.querySelector("header");
+  header.classList.remove('active');
+}
+
